5a58d5a936e605ae4e16bf24b4ed74ac6583a21f
Daniel Deming <dand@ghostcave.home>
2014-02-14 11:50:35 -0500

    Set up board state
    Define an init function to set up initial state
    of the life board

--- jasmine-standalone-2.0.0/spec/LifeSpec.js
+++ jasmine-standalone-2.0.0/spec/LifeSpec.js
@@ -1,5 +1,26 @@
-describe("Life class", function() {
-  it("should be defined", function() {
-    expect(life).toBeDefined();
-  });
+describe('Life class', function() {
+    it('should be defined', function() {
+      expect(life).toBeDefined();
+    });
+
+    describe('init function', function() {
+        it('should be defined', function() {
+            expect(typeof life.init).toBe('function');
+        });
+
+        it('should set game board size', function() {
+            life.init({board: {x:5, y:5}});
+            expect(life.getBoard()['x']).toBe(5);
+        });
+
+        it('should accept game state', function() {
+          life.init({on: [
+              { x: 3, y: 4 },
+              { x: 3, y: 3 },
+              { x: 3, y: 2 }
+          ]});
+          expect(life.getCurrentOn()[0].x).toBe(3);
+        });
+    });
+
 });
--- jasmine-standalone-2.0.0/src/life.js
+++ jasmine-standalone-2.0.0/src/life.js
@@ -1,8 +1,41 @@
 /**
  * Global life class houses all application logic
  */
-var life = (function() {
-    return {
+var life = (function life() {
 
+    /**
+     * Protected Vars
+     */
+    var maxX = 10,
+        maxY = 10,
+        currentOn = [];
+
+    return {
+        getBoard: function() {
+            return {x: maxX, y: maxY};
+        },
+
+        getCurrentOn: function() {
+            return currentOn;
+        },
+
+        /**
+         * Define an initial life board
+         * @param {Object} args. Supports the following:
+         *   {
+         *     board: { x: 5, y: 5}, // creates a 5x5 board
+         *     on: [
+         *         { x: 3, y: 4 },   // specifies all points
+         *         { x: 3, y: 3 },   // on the board that are "on"
+         *         { x: 3, y: 2 },
+         *     ]
+         *   }
+         */
+        init: function(args) {
+            if (args.board && args.board.x) maxX = args.board.x;
+            if (args.board && args.board.y) maxY = args.board.y;
+            if (args.on && args.on) currentOn = args.on;
+        }
     };
+
 })();
